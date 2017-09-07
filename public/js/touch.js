4fab23a2e40fc8507ab61b626b749c1212417c67
myprelude <1142929692@qq.com>
2017-09-07 15:51:21 +0800

    no message

--- public/js/touch.js
+++ public/js/touch.js
@@ -1,12 +1,24 @@
 !function(){
-	
+	var support = support_css();
+	var transform = support?support+'Transform':'transform';
+	var transition= support?support+'Transition':'transition';
+	function support_css(){
+		var div = document.createElement('div');
+		var vendors =['Ms','O','Moz','Webkit','']; 
+		for(var i=0,prop;i<vendors.length;i++){
+			prop = vendors[i]?'Transform':'transform';
+			if(vendors[i]+prop in div.style){
+				return vendors[i]
+			}
+		}
+	}
 	function Touch(option){
 		return new Touch.prototype.init(option);
 	}
 	Touch.prototype={
 		constructor:Touch,
 		init:function(option){
-			this.el = option.el;
+			this.el = typeof option.el=='string'?document.querySelector(option.el):option.el;
 			this.target = option.target;
 			this.callback =option.callback;
 			this.direction =option.direction || 'width';
@@ -22,32 +34,21 @@
 			var startx,starty,startt1,endx,endy,endt1,status=0,num;
 			var wid = parseFloat(getStyle(_this.el,_this.direction));
 			on(_this.el,'touchstart',function(e){
-				e.stopPropagation();
-				e.preventDefault();
+				event(e);
 				num = _this.target.querySelectorAll('div').length;
-				startx=e.changedTouches[0].clientX;
-				starty=e.changedTouches[0].clientY;
-				_this.target.style.transition='none';
-				_this.target.style.transform=_this.direction=='height'?'translate3d(0,'+wid*status+'px,0)':'translate3d('+wid*status+'px,0,0)';
-				startt1=new Date().getTime();
+				_this.target.style[transition]='none';
+				_this.target.style[transform]=_this.direction=='height'?'translate3d(0,'+wid*status+'px,0)':'translate3d('+wid*status+'px,0,0)';
+				
 			})
 			on(_this.el,'touchmove',function(e){
-				e.stopPropagation();
-				e.preventDefault();
-				endx=e.changedTouches[0].clientX;
-				endy=e.changedTouches[0].clientY;
-				endt1=new Date().getTime();
+				event(e);
 				var s = _this.direction=='height'?endy-starty:endx-startx;
 				var t = endt1-startt1;
 				_this.callback&&_this.callback(s+wid*status,t,status);
 			})
 			on(_this.el,'touchend',function(e){
-				e.stopPropagation();
-				e.preventDefault();
-				_this.target.style.transition='all '+_this.time+' ease';
-				endx=e.changedTouches[0].clientX;
-				endy=e.changedTouches[0].clientY;
-				endt1=new Date().getTime();
+				event(e);
+				_this.target.style[transition]='all '+_this.time+' ease';
 				var s = _this.direction=='height'?endy-starty:endx-startx;
 				var t = endt1-startt1;
 				var dir = s>0?1:-1;
@@ -63,6 +64,20 @@
 				}
 				_this.callback&&_this.callback(wid*status,t,status);
 			})
+
+			function event(e){
+				e.stopPropagation();
+				e.preventDefault();
+				if(e.type==='touchstart'){
+					startx=e.changedTouches[0].clientX;
+					starty=e.changedTouches[0].clientY;
+					startt1=new Date().getTime();
+				}else{
+					endx=e.changedTouches[0].clientX;
+					endy=e.changedTouches[0].clientY;
+					endt1=new Date().getTime();
+				}
+			}
 		}
 	}
 	function on (el,type,callback){
