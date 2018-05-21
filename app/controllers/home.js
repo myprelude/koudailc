7305471ab485304a53669ddde1f32edb1aa476d2
unknown <1142929692@qq.com>
2018-05-22 00:40:08 +0800

    初步完成

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -28,6 +28,11 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
+router.get('/', function (req, res, next) {
+  res.render('index');
+
+});
+
 router.get('/edit', function (req, res, next) {
     res.render('edit');
 //   var count = 0,show;
